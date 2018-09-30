35e6ded4c95bb406ed4d43f669c14e23fde9767f
Grant Maloney <grantmaloney@grants-mbp.centurylink.net>
2018-09-30 15:09:28 -0500

    More radar updates

--- Weather/RadarViewController.swift
+++ Weather/RadarViewController.swift
@@ -18,8 +18,8 @@ class RadarViewController: AWFWeatherMapViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let layer = AWFRasterMapLayer(layerType: .radar)
-        weatherMap.amp.addRasterLayer(layer)
+        //let layer = AWFRasterMapLayer(layerType: .radar)
+        //weatherMap.amp.addRasterLayer(layer)
         if let lat = latitude {
             if let lng = longitude {
                 let weather = weatherMap.mapView as! MKMapView
@@ -27,6 +27,24 @@ class RadarViewController: AWFWeatherMapViewController {
                 let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.50, longitudeDelta: 0.50))
                 weather.setRegion(region, animated: true)
             }
+        }
+        
+        legendView.addLegend(forLayerType: AWFMapLayer.radar)
+        self.navigationItem.rightBarButtonItem = nil
+        
+        let mainSubView = self.view.subviews[3];
+        
+        /*
+         This is really gross, this API documentation and manipulability is AWFUL. Either I don't understand some functionality, or they wrote it to be as uncustomizable as possible. I feel the later, I've spent 2 days now trying to manipulate the view to my liking. This was my best solution and making the legend view look clean.
+        */
+        if let stackView = mainSubView.subviews[0] as? UIStackView {
+            if let label = stackView.subviews[0].subviews[0] as? UILabel {
+                label.text = ""
+            }
+        }
+        
+        if let button = mainSubView.subviews[1] as? UIButton {
+            button.sendActions(for: .touchUpInside)
         }
     }
 }
